8d0a24f2e45162c47b32d9f00836e872d4a33ace
leead4 <leead4@gmail.com>
2017-08-20 15:54:59 -0500

    added toasts

--- js/controllers/practice_controller.js
+++ js/controllers/practice_controller.js
@@ -14,6 +14,7 @@ app.controller('PracticeCtrl', function($scope, $window){
     $scope.execute = () => {
         buildFunction();
     };
+    
 
     let buildFunction = () => {
         parseFunction($scope.functionDef);
@@ -37,6 +38,7 @@ app.controller('PracticeCtrl', function($scope, $window){
         if (f === 'function'){
             extractFunctionName(string);
         } else {
+            Materialize.toast('You have spelled function incorrectly', 5000);
             console.log('TOOLTIP: You have spelled function incorrectly');
         }
     };
@@ -45,6 +47,7 @@ app.controller('PracticeCtrl', function($scope, $window){
         if (string.includes('()') || string.includes('( )')) {
             checkCurlies(string);
         } else {
+            Materialize.toast('You have not formatted your function definition correctly. Please check your ()', 5000);
             console.log('TOOLTIP: You have not formatted your function definition correctly. Please check your ()');
         }
     };
@@ -53,6 +56,7 @@ app.controller('PracticeCtrl', function($scope, $window){
         if (string.includes('{') && string.includes('}')) {
             getReturn(string);
         } else {
+            Materialize.toast('You are missing an opening or closing curly brace.', 5000);
             console.log('You are missing an opening or closing curly brace.');
         }
     };
@@ -65,6 +69,7 @@ app.controller('PracticeCtrl', function($scope, $window){
       withReturns.splice(withReturns.indexOf("\n"), 1);
 
       if (withReturns[(withReturns.indexOf("\n"))-1] !== ";") {
+        Materialize.toast("Don't forget your ';' at the end of your return statement!", 5000);
         console.log("TOOL TIP: Don't forget your ';' at the end of your return statement!");
       } else {
         withReturns = withReturns.join('');
@@ -75,10 +80,13 @@ app.controller('PracticeCtrl', function($scope, $window){
 
     };
 
+    
+
     let checkFunctionCall = () => {
         if ($scope.functionCall === `${functionName}()`){
             runUserFunction();
         } else {
+            Materialize.toast('You have called the function incorrectly.', 5000)
             console.log("TOOLTIP: You have called the function incorrectly.");
         }
     };
@@ -88,5 +96,6 @@ app.controller('PracticeCtrl', function($scope, $window){
         let definition = new Function(userReturn);
         $scope.functionResult = definition();
     };
+
 
 });
