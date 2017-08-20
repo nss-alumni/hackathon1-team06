fae89c9f5a9e4c62f563f0c617e0fb4a56bb6a33
jevonthomas <jevon.g.thomas@gmail.com>
2017-08-20 10:09:30 -0500

    structure the index file to support our app in angular-materialize; set up the routing for the home page in app.js; created the home page with a splash photo and the introductory get song example

---
+++ js/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let app = angular.module("TotalEclipse", ["ngRoute", "ui.materialize"]);
+
+
+app.config( ($routeProvider) => {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/home.html',
+    controller: 'HomeController'
+  })
+  .otherwise('/');
+});
